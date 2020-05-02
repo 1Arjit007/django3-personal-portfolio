e0f44aeeedc9bf3315005a280bc34dc93987bf5d
Arjit <1arjit.kesarwani@gmail.com>
2020-05-02 12:34:40 +0530

    Added te resume

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
+'''try:
+    from .local_settings import *
+except ImportError:
+    print("looks like no local file")'''
